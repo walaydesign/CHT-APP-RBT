0719d7da3cbc47a4e02990d69d36b29e05774cbd
youzhen <yl301114@gmail.com>
2025-09-26 11:50:23 +0100

    快完成9/26

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,8 @@
 $(".song-pic_icon").click(function () {
     $(this).parents(".song-pic").toggleClass("playing");
 });
+
+$(".produce-sec_head").click(function () {
+    $(this).find(".produce-sec_icon").toggleClass("active");
+    $(this).parents(".produce-sec").find(".produce-sec_body").slideToggle(300);
+});
